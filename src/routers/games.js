8b2ca997a9a0b5238ace2561ef0bdf9e597b9e03
encarnacao <cencarnacaoq@gmail.com>
2023-02-10 11:48:18 -0300

    refactor: rename conflict function

--- src/routers/games.js
+++ src/routers/games.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { getGames, postGame } from "../controllers/games.js";
-import { validateGame, checkConflicts } from "../middlewares/games.js";
+import { validateGame, checkGameConflict } from "../middlewares/games.js";
 
 const router = Router();
 
 router.get("/games", getGames);
-router.post("/games", validateGame, checkConflicts, postGame);
+router.post("/games", validateGame, checkGameConflict, postGame);
 
 export default router;
