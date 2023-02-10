ca830f215922b28c5d67a9973aea81ea63d0cdd5
encarnacao <cencarnacaoq@gmail.com>
2023-02-10 17:47:04 -0300

    fix: remove unused properties from rentalSchema

--- src/schemas/rentals.js
+++ src/schemas/rentals.js
@@ -1,16 +1,9 @@
-import JoiBase from "joi";
-import JoiDate from "@joi/date";
-
-const Joi = JoiBase.extend(JoiDate);
+import Joi from "joi";
 
 const rentalSchema = Joi.object({
 	customerId: Joi.number().integer().required(),
 	gameId: Joi.number().integer().required(),
-	rentDate: Joi.date().format("YYYY-MM-DD").required(),
 	daysRented: Joi.number().integer().required(),
-	returnDate: Joi.date().format("YYYY-MM-DD"),
-	originalPrice: Joi.number().integer().required(),
-	delayFee: Joi.number().integer(),
 });
 
 export default rentalSchema;
