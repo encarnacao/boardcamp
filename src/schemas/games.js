2860c27a68f97392525b81c926d13d9661f16d33
encarnacao <cencarnacaoq@gmail.com>
2023-02-09 23:22:42 -0300

    feat: check if name is non-empty and stockTotal and pricePerDay are greater than 0

--- src/schemas/games.js
+++ src/schemas/games.js
@@ -1,10 +1,10 @@
 import Joi from "joi";
 
 const gameSchema = Joi.object({
-    name: Joi.string().required(),
+    name: Joi.string().min(1).required(),
     image: Joi.string().required(),
-    stockTotal: Joi.number().integer().required(),
-    pricePerDay: Joi.number().integer().required(),
+    stockTotal: Joi.number().integer().greater(0).required(),
+    pricePerDay: Joi.number().integer().greater(0).required(),
 });
 
 export default gameSchema;
