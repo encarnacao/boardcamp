936c3186d1afa776d42239c36480067625c6365d
encarnacao <cencarnacaoq@gmail.com>
2023-02-10 17:48:20 -0300

    fix: return proper status code

--- src/middlewares/rentals.js
+++ src/middlewares/rentals.js
@@ -4,7 +4,7 @@ function validateRental(req, res, next) {
 	const { error } = rentalSchema.validate(req.body);
 	if (error) {
 		const errorMessage = error.details.map((err) => err.message).join(", ");
-		return res.status(422).send(errorMessage);
+		return res.status(400).send(errorMessage);
 	}
 	next();
 }
