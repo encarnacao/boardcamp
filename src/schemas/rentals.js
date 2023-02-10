f3396a97166003ccf33db77938e48063403deea0
encarnacao <cencarnacaoq@gmail.com>
2023-02-10 18:16:53 -0300

    feat: check if daysRented is greater than 0

--- src/schemas/rentals.js
+++ src/schemas/rentals.js
@@ -3,7 +3,7 @@ import Joi from "joi";
 const rentalSchema = Joi.object({
 	customerId: Joi.number().integer().required(),
 	gameId: Joi.number().integer().required(),
-	daysRented: Joi.number().integer().required(),
+	daysRented: Joi.number().integer().greater(0).required(),
 });
 
 export default rentalSchema;
