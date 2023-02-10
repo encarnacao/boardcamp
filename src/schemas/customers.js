e5fad3ecfeb251d6b6676390aec560aea47e64ed
encarnacao <cencarnacaoq@gmail.com>
2023-02-10 11:37:03 -0300

    feat: add restrictions to phone and cpf string

--- src/schemas/customers.js
+++ src/schemas/customers.js
@@ -5,8 +5,8 @@ const Joi = JoiBase.extend(JoiDate);
 
 const customerSchema = Joi.object({
 	name: Joi.string().required(),
-	phone: Joi.string().required(),
-	cpf: Joi.string().length(11).required(),
+	phone: Joi.string().min(11).max(12).regex(/^\d+$/).required(),
+	cpf: Joi.string().length(11).regex(/^\d+$/).required(),
 	birthday: Joi.date().format("YYYY-MM-DD").required(),
 });
 
