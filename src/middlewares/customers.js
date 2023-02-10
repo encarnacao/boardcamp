80d4cf09782afd4b33bdd1dd6d5a3364083dc395
encarnacao <cencarnacaoq@gmail.com>
2023-02-10 10:53:37 -0300

    feat: check for conflicts on the database

--- src/middlewares/customers.js
+++ src/middlewares/customers.js
@@ -1,3 +1,4 @@
+import { db } from "../database.connection.js";
 import customerSchema from "../schemas/customers.js";
 
 function validateCustomer(req, res, next) {
@@ -7,6 +8,23 @@ function validateCustomer(req, res, next) {
 		return res.status(422).send(errorMessage);
 	}
 	next();
+}
+
+async function checkConflict(req, res, next) {
+	try {
+		const { cpf } = req.body;
+		const customer = await db.query(
+			`SELECT * FROM customers WHERE cpf = $1`,
+			[cpf]
+		);
+		if (customer.rowCount > 0) {
+			return res.sendStatus(409);
+		}
+		next();
+	} catch {
+		console.log(error);
+		res.sendStatus(500);
+	}
 }
 
 export { validateCustomer };
