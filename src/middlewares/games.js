8c27287701c0d98966215131abbbe0e38b68ec44
encarnacao <cencarnacaoq@gmail.com>
2023-02-09 23:23:06 -0300

    feat: check for conflicts on the database

--- src/middlewares/games.js
+++ src/middlewares/games.js
@@ -1,12 +1,22 @@
+import { db } from "../database.connection.js";
 import gameSchema from "../schemas/games.js";
 
 function validateGame(req, res, next) {
 	const { error } = gameSchema.validate(req.body);
 	if (error) {
 		const errorMessage = error.details.map((err) => err.message).join(", ");
-		return res.status(422).send(errorMessage);
+		return res.status(400).send(errorMessage);
 	}
 	next();
 }
 
-export { validateGame };
+async function checkConflicts(req, res, next) {
+    const { name } = req.body;
+    const games = await db.query("SELECT * FROM games WHERE name = $1", [name]);
+    if (games.rowCount > 0) {
+        return res.status(409).send("Game already exists in the database.");
+    }
+    next();
+}
+
+export { validateGame, checkConflicts };
