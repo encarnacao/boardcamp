291f32286dacbab175a315db729360a59a508a63
encarnacao <cencarnacaoq@gmail.com>
2023-02-10 11:03:01 -0300

    feat: add post customer controller

---
+++ src/controllers/customers.js
@@ -0,0 +1,17 @@
+import { db } from "../database.connection";
+
+async function postCustomer(req, res) {
+	try {
+		const { name, phone, cpf, birthday } = req.body;
+		await db.query(
+			"INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4)",
+			[name, phone, cpf, birthday]
+		);
+		res.sendStatus(201);
+	} catch (error) {
+		console.log(error);
+		res.sendStatus(500);
+	}
+}
+
+export { postCustomer };
