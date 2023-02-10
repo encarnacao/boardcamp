0f2e5c5ff29ecc2d071b4759e7c396b0ff625c08
encarnacao <cencarnacaoq@gmail.com>
2023-02-09 22:28:40 -0300

    feat: add game validation middleware

---
+++ src/middlewares/games.js
@@ -0,0 +1,12 @@
+import gameSchema from "../schemas/games";
+
+function validateGame(req, res, next) {
+	const { error } = gameSchema.validate(req.body);
+	if (error) {
+		const errorMessage = error.details.map((err) => err.message).join(", ");
+		return res.status(422).send(errorMessage);
+	}
+	next();
+}
+
+export { validateGame };
