01eb290fda49c191808dadce27d0f939342c6bdd
encarnacao <cencarnacaoq@gmail.com>
2023-02-09 22:28:15 -0300

    feat: add customer validation middleware

---
+++ src/middlewares/customers.js
@@ -0,0 +1,12 @@
+import customerSchema from "../schemas/customers";
+
+function validateCustomer(req, res, next) {
+	const { error } = customerSchema.validate(req.body);
+	if (error) {
+		const errorMessage = error.details.map((err) => err.message).join(", ");
+		return res.status(422).send(errorMessage);
+	}
+	next();
+}
+
+export { validateCustomer };
