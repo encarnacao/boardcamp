4262f984fc31428b6f788ceb80fee32124c64778
encarnacao <cencarnacaoq@gmail.com>
2023-02-10 11:02:16 -0300

    feat: check date format for 'YYYY-MM-DD'

--- src/schemas/customers.js
+++ src/schemas/customers.js
@@ -1,10 +1,13 @@
-import Joi from "joi";
+import JoiBase from "joi";
+import JoiDate from "@joi/date";
+
+const Joi = JoiBase.extend(JoiDate);
 
 const customerSchema = Joi.object({
-    name: Joi.string().required(),
-    phone: Joi.string().required(),
-    cpf: Joi.string().length(11).required(),
-    birthday: Joi.date().required(),
+	name: Joi.string().required(),
+	phone: Joi.string().required(),
+	cpf: Joi.string().length(11).required(),
+	birthday: Joi.date().format("YYYY-MM-DD").required(),
 });
 
 export default customerSchema;
--- src/schemas/rentals.js
+++ src/schemas/rentals.js
@@ -1,13 +1,16 @@
-import Joi from "joi";
+import JoiBase from "joi";
+import JoiDate from "@joi/date";
+
+const Joi = JoiBase.extend(JoiDate);
 
 const rentalSchema = Joi.object({
-    customerId: Joi.number().integer().required(),
-    gameId: Joi.number().integer().required(),
-    rentDate: Joi.date().required(),
-    daysRented: Joi.number().integer().required(),
-    returnDate: Joi.date(),
-    originalPrice: Joi.number().integer().required(),
-    delayFee: Joi.number().integer(),
+	customerId: Joi.number().integer().required(),
+	gameId: Joi.number().integer().required(),
+	rentDate: Joi.date().format("YYYY-MM-DD").required(),
+	daysRented: Joi.number().integer().required(),
+	returnDate: Joi.date().format("YYYY-MM-DD"),
+	originalPrice: Joi.number().integer().required(),
+	delayFee: Joi.number().integer(),
 });
 
 export default rentalSchema;
