28d1f314fb705fd57ef78752bb69a391eb5f99c8
encarnacao <cencarnacaoq@gmail.com>
2023-02-11 15:30:00 -0300

    feat: add search through query string on get games

--- src/controllers/games.js
+++ src/controllers/games.js
@@ -1,8 +1,14 @@
 import { db } from "../database.connection.js";
+import { escape } from "mysql";
 
-async function getGames(_, res) {
+async function getGames(req, res) {
 	try {
-		const games = await db.query("SELECT * FROM games");
+		const { name } = req.query;
+		let query = "SELECT * FROM games";
+		if (name) {
+			query += ` WHERE name ILIKE ${escape(`%${name}%`)}`;
+		}
+		const games = await db.query(query);
 		res.status(200).send(games.rows);
 	} catch (error) {
 		console.log(error);
